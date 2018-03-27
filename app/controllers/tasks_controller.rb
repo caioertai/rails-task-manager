4f00522c49c017b3cc45f94ac1f3f76533b4a987
Caio Andrade <caioertai@gmail.com>
2018-03-27 13:53:25 -0300

    Before action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :find_task_id, only: [:show, :edit, :update, :toggle, :destroy]
+
   def new
     @task = Task.new
   end
@@ -14,28 +16,23 @@ class TasksController < ApplicationController
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
     @task.update(task_params)
     redirect_to root_path
   end
 
   def toggle
-    @task = Task.find(params[:id])
     new_value = !@task.completed
     @task.update_attribute(:completed, new_value)
     redirect_to root_path
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     redirect_to root_path
   end
@@ -44,5 +41,9 @@ class TasksController < ApplicationController
 
   def task_params
     params.require(:task).permit(:name, :details)
+  end
+
+  def find_task_id
+    @task = Task.find(params[:id])
   end
 end
